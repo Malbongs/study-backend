7b5eb8dfeb1e79973cb1faeb5f2a5753f77f04d5
Malbongs <qothgus13@naver.com>
2022-08-11 17:47:33 +0900

    다시수정

--- app.js
+++ app.js
@@ -7,13 +7,6 @@ app.use( '/static', express.static('static')); // 접근하기 위해서  /stati
 const port = 8000; //8000번 포트로 연결
 //ip:8000/
 app.get("/",(req,res)=>{//다음 주소를 정해주는 문자열 req(request약자)클라이언트가 사버에게    res(restuns)서버가 클라이언트한테 보내는 응답
-//    var person = [
-//     {name: "박나래", job: "개그맨",gender:"여"},
-//     {name: "김범수", job: "가수",gender:"남"},
-//     {name: "박재범", job: "가수",gender:"남"},
-//     {name: "하정우", job: "배우",gender:"남"},
-//     {name: "전지현", job: "배우",gender:"여"},
-//    ]
     res.render("a"); 
 }) 
 // app.get("/test",(req,res)=>{
